b4a6496166130d1615c62ddbc5e129f66f460fce
skpatil <skpatil@gmail.com>
2024-02-16 14:05:31 +0530

    Removed for - single loop

--- PrintAlphabests.java
+++ PrintAlphabests.java
@@ -2,9 +2,6 @@ public class PrintAlphabests {
     public static void main(String[] args) {
         for(int i =0; i < 26; i++){
             System.out.print((char)(i+65));
-        }
-        System.out.println();
-        for(int i =0; i < 26; i++){
             System.out.print((char)(i+97));
         }
     }
