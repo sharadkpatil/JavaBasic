6a4864c9987a42aac17734d95e7abf4bc73f5f45
skpatil <skpatil@gmail.com>
2024-02-16 13:03:17 +0530

    FirstJava Git push test

--- test.java
+++ test.java
@@ -1,5 +1,8 @@
 class test {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
+        for (int i=1; i < 11 ; i++) {
+            System.out.println("I = " + i);
+        }
     }
 }
