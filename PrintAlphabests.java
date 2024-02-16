70516644f279b049db740e436708a251647506c5
skpatil <skpatil@gmail.com>
2024-02-16 14:09:24 +0530

    Print Alphabets

--- PrintAlphabests.java
+++ PrintAlphabests.java
@@ -1,5 +1,6 @@
 public class PrintAlphabests {
     public static void main(String[] args) {
+        System.out.println("Print Alphbets");
         for(int i =0; i < 26; i++){
             System.out.print((char)(i+65));
         }
